fb42e73af206a89fc454febf7db08a0fbb74b2ee
Christo Crampton <info@38.co.za>
2013-08-13 23:50:24 +0200

    search app is a little more generic (with configurations), more templatetags, some ui tweaks

--- haystackui/templatetags/haystackui.py
+++ haystackui/templatetags/haystackui.py
@@ -15,6 +15,26 @@ def get_active_facet_filters(request):
     """
     qs_facets = request.GET.getlist("selected_facets")
     return [value.split(":")[1] for value in qs_facets]
+
+@register.filter(name='facet_for_humans')
+def facet_for_humans(facet):
+    return facet.split(":")[1]
+
+
+@register.filter(name='remove_facet')
+def remove_facet(path, facet):
+
+    try:
+        url, qs = path.split("?")
+        facet_to_remove = "selected_facets=%s" % facet
+        key_values = qs.split("&")
+        if facet_to_remove in key_values: key_values.remove(facet_to_remove)
+
+        return "%s?%s" % (url, ("&").join(key_values) )
+
+    except ValueError:
+        return path
+    
     
     
 
--- haystackui/urls.py
+++ haystackui/urls.py
@@ -4,8 +4,20 @@ from haystack.forms import ModelSearchForm, FacetedSearchForm
 from haystack.query import SearchQuerySet
 from haystack.views import SearchView, FacetedSearchView, search_view_factory
 
-from demo.models import Repo
+from demo.models import Repo, Author
 
+url_configs = [
+    {
+        "url_fragment" : "repos",
+        "facets" : ["author", "tag"],
+        "obj" : Repo
+    },
+    {
+        "url_fragment" : "authors",
+        "facets" : [],
+        "obj" : Author
+    }    
+]
 
 urlpatterns = patterns('',
 # haystack search:
@@ -13,7 +25,24 @@ urlpatterns = patterns('',
     # /search/<model>/ => include 
     url(r'^$', FacetedSearchView(
                         form_class=FacetedSearchForm, 
-                        searchqueryset=SearchQuerySet().all().models(Repo).facet("author").facet("tag")), 
-            
+                        searchqueryset=SearchQuerySet().all().facet("author").facet("tag")),             
             name='haystack_faceted_search'),
+
+    url(r'^autocomplete/$', 'haystackui.views.autocomplete', name='haystack_autocomplete_search'),
 )
+
+for url_config in url_configs:
+
+    url_fragment = url_config.get("url_fragment")
+    sqs = SearchQuerySet().all().models(url_config.get("obj"))
+
+    for facet in url_config.get("facets",[]):
+        sqs = sqs.facet(facet)
+
+    urlpatterns += patterns('',
+                    url( r'^%s/$' % url_fragment, FacetedSearchView(
+                            form_class=FacetedSearchForm, 
+                            searchqueryset=sqs),             
+                        name='haystack_%s_faceted_search' % url_fragment) 
+    )
+    
